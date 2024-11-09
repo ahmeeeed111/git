47755431a45a11b4857d6bde82e613153b93a272
idiahmed <idiahmed5@gmail.com>
2024-11-09 15:54:39 +0100

    Version1

---
+++ Devisionbyneg.java
@@ -0,0 +1,10 @@
+package revision;
+
+class Devisionbyneg extends Exception{
+	
+	public Devisionbyneg(String error)
+	{
+		super(error);
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,46 @@
+package revision;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	 BufferedReader reader=null;
+	 String FilePath = "C:\\Users\\ahmed\\Desktop\\vergleich\\00.txt";
+
+     try
+     {
+    	 reader=new BufferedReader(new FileReader(FilePath));
+    	 String next;
+    	 while((next=reader.readLine())!=null)
+    	 {
+    		 System.out.println(next); 
+    	 }
+    	 
+     }
+     catch(IOException e)
+     {
+    	 System.out.println("File import error");
+     }
+     finally
+     {
+    	 if(reader!=null)
+    	 {
+    		 try
+    		 {
+    			 reader.close();
+    		 }
+    		 catch(Exception e)
+    		 {
+    			 System.out.println("Error");
+    		 }
+    	 }
+     }
+	}
+	
+
+}
